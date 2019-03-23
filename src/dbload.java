6dd2d16ba0c435ea03d155ca7fdc9ff369a53e3d
keKing000 <282989088@qq.com>
2019-03-23 13:48:10 +0800

    说明

--- src/dbload.java
+++ src/dbload.java
@@ -2,7 +2,8 @@ import operate.Write;
 
 public class dbload {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        //TimeUnit.SECONDS.sleep(5);
         if (args.length != 3) {
             System.out.println("The parameter must have three");
             return;
